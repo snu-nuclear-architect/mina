35d7f05cd9f51ad81b07b1b43292f56db87eecaa
chenxiangyi <chenxiangyi@snu.ac.kr>
2016-05-10 11:40:45 +0900

    upload the Initializion file for you

---
+++ Initial_condition.py
@@ -0,0 +1,6 @@
+class Initial_condition():
+    def __init__(self,T_initial, P_initial, Mass_flow_rate_initial):
+        self.T_out = T_initial
+        self.P_out = P_initial
+        self.mass_flow_rate_out = Mass_flow_rate_initial
+
